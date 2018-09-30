af0d00d250db6d16fbbbc34a59ee43f2a066824f
RaamanKilaru <kilaru.raaman@gmail.com>
2018-09-30 10:07:08 +0000

    Actually Swapping.

---
+++ swap.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+
+int main(){
+    int a,b;
+    printf("Enter a and b : \n");
+    scanf("%d %d",&a,&b);
+    printf("Before Swapping.\n");
+    printf("a : %d  b : %d\n",a,b);
+
+    //swapping
+
+    printf("After Swapping.\n");
+    printf("a : %d  b : %d\n",a,b);
+    return 0;
+}
